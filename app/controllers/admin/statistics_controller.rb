c36302553a8f69c9c7886518fe6b2fa2c81af755
Arnelle Balane <arnellebalane@gmail.com>
2015-01-17 20:08:03 +0800

    updated statistics page data display

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -4,9 +4,14 @@ class Admin::StatisticsController < ApplicationController
 
   def index
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
-    @offices = Office.alive
-    @total_pending = Document.alive.where("status = 1 AND DATE(created_at) = DATE(:date)", { date: @date }).count
-    @total_completed = Document.alive.where("status = 2 AND DATE(created_at) = DATE(:date)", { date: @date }).count
-    @total_rejected = Document.alive.where("status = 3 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @offices = Office.alive.order("id")
+    @own_total_pending = Document.alive.where("status = 1 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @own_total_completed = Document.alive.where("status = 2 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @own_total_rejected = Document.alive.where("status = 3 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @routed_total_incoming = DocumentRoute.where("status = 0 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @routed_total_pending = DocumentRoute.where("status = 1 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @routed_total_released = DocumentRoute.where("status = 2 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @routed_total_completed = DocumentRoute.where("status = 4 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @routed_total_rejected = DocumentRoute.where("status = 3 AND DATE(created_at) = DATE(:date)", { date: @date }).count
   end
 end
