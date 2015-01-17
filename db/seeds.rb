b0e2e0a56686bb42e2078bbf01f39a7a9b360918
Arnelle Balane <arnellebalane@gmail.com>
2015-01-17 20:51:55 +0800

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,40 @@
 Office.create(
   name: "Department Of Computer Science",
-  email: "arnelle@gmail.com",
+  email: "arnellebalane@gmail.com",
   password: "password",
   admin: true
 );
 
-Office.create(
-  name: "Accounting Office",
-  email: "accounting@gmail.com",
-  password: "password"
-)
+# Office.create(
+#   name: "Accounting Office",
+#   email: "accounting@gmail.com",
+#   password: "password"
+# )
 
-Office.create(
-  name: "Dean's Office",
-  email: "dean@gmail.com",
-  password: "password"
-)
+# Office.create(
+#   name: "Dean's Office",
+#   email: "dean@gmail.com",
+#   password: "password"
+# )
 
-first = Office.first
-first.office_staffs.create(name: "Arnelle Balane")
-first.office_staffs.create(name: "Julia Camille Menchavez")
-first.office_staffs.create(name: "Emmanuel Lodovice")
-first.office_staffs.create(name: "Paulinko Kiel Labian")
-first.office_staffs.create(name: "Kevin Calingacion")
+# first = Office.first
+# first.office_staffs.create(name: "Arnelle Balane")
+# first.office_staffs.create(name: "Julia Camille Menchavez")
+# first.office_staffs.create(name: "Emmanuel Lodovice")
+# first.office_staffs.create(name: "Paulinko Kiel Labian")
+# first.office_staffs.create(name: "Kevin Calingacion")
 
-DocumentType.create(
-  name: "Purchase Request",
-  route: "1,2,3"
-)
+# DocumentType.create(
+#   name: "Purchase Request",
+#   route: "1,2,3"
+# )
 
-DocumentType.create(
-  name: "Letter",
-  route: "1,3"
-)
+# DocumentType.create(
+#   name: "Letter",
+#   route: "1,3"
+# )
 
-DocumentType.create(
-  name: "Leave",
-  route: "2,3"
-)
+# DocumentType.create(
+#   name: "Leave",
+#   route: "2,3"
+# )
