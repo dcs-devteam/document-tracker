a4f7c4c999ca3e650a6ca3e614d00c05505271b8
Arnelle Balane <arnellebalane@gmail.com>
2015-01-17 14:29:18 +0800

    reversed order listing in routed documents

--- app/controllers/office/documents_controller.rb
+++ app/controllers/office/documents_controller.rb
@@ -2,17 +2,17 @@ class Office::DocumentsController < DocumentsController
   def index
     case params[:filter]
     when "incoming"
-      @documents = current_office.document_routes.incoming
+      @documents = current_office.document_routes.incoming.order("-id")
     when "received"
-      @documents = current_office.document_routes.received
+      @documents = current_office.document_routes.received.order("-id")
     when "released"
-      @documents = current_office.document_routes.released
+      @documents = current_office.document_routes.released.order("-id")
     when "completed"
-      @documents = current_office.document_routes.completed
+      @documents = current_office.document_routes.completed.order("-id")
     when "rejected"
-      @documents = current_office.document_routes.rejected
+      @documents = current_office.document_routes.rejected.order("-id")
     else
-      @documents = current_office.document_routes
+      @documents = current_office.document_routes.order("-id")
     end
     @documents = @documents.map(&:document).uniq.select { |document| !document.erased }
   end
