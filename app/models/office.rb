ce2b534853511b137a7e954adc560396f89d0363
Arnelle Balane <arnellebalane@gmail.com>
2015-01-17 20:25:10 +0800

    office turnaround time

--- app/models/office.rb
+++ app/models/office.rb
@@ -26,4 +26,31 @@ class Office < ActiveRecord::Base
   def toggle_admin!
     self.update! admin: !self.admin
   end
+
+  def turnaround_time
+    documents = self.document_routes.released + self.document_routes.completed + self.document_routes.rejected
+    if documents.any?
+      total = 0
+      documents.each do |document|
+        total += document.date_out - document.date_in
+      end
+      total /= documents.count
+      seconds = total.round
+      if seconds >= 60
+        minutes = seconds % 60
+        seconds /= 60
+      end
+      if seconds >= 60
+        hours = minutes % 60
+        minutes /= 60
+      end
+
+      time = []
+      time << "#{hours} hours" if hours
+      time << "#{minutes} minutes" if minutes
+      time << "#{seconds} seconds" if seconds
+      return time.join ", "
+    end
+    return "Cannot be determined"
+  end
 end
