1b403fc159ef4a42e05383c4f713e8207f0d602c
Arnelle Balane <arnellebalane@gmail.com>
2015-03-04 12:22:28 +0800

    fianl fix to statistics page

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -3,34 +3,32 @@ class Admin::StatisticsController < ApplicationController
   before_filter :require_admin_access
 
   def index
-    if Office.alive.any?
-        @date = params[:date] ? Date.parse(params[:date]) : Date.today
-        @offices = Office.alive.order("id")
-        @own_total_pending = Document.alive.where("status = 1 AND DATE(created_at) = DATE(:date)", { date: @date }).count
-        @own_total_completed = Document.alive.where("status = 2 AND DATE(created_at) = DATE(:date)", { date: @date }).count
-        @own_total_rejected = Document.alive.where("status = 3 AND DATE(created_at) = DATE(:date)", { date: @date }).count
-        @routed_total_incoming = DocumentRoute.where("status = 0 AND DATE(created_at) = DATE(:date)", { date: @date }).count
-        @routed_total_pending = DocumentRoute.where("status = 1 AND DATE(created_at) = DATE(:date)", { date: @date }).count
-        @routed_total_released = DocumentRoute.where("status = 2 AND DATE(created_at) = DATE(:date)", { date: @date }).count
-        @routed_total_completed = DocumentRoute.where("status = 4 AND DATE(created_at) = DATE(:date)", { date: @date }).count
-        @routed_total_rejected = DocumentRoute.where("status = 3 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @offices = Office.alive.order("id")
+    @own_total_pending = Document.alive.where("status = 1 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @own_total_completed = Document.alive.where("status = 2 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @own_total_rejected = Document.alive.where("status = 3 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @routed_total_incoming = DocumentRoute.where("status = 0 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @routed_total_pending = DocumentRoute.where("status = 1 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @routed_total_released = DocumentRoute.where("status = 2 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @routed_total_completed = DocumentRoute.where("status = 4 AND DATE(created_at) = DATE(:date)", { date: @date }).count
+    @routed_total_rejected = DocumentRoute.where("status = 3 AND DATE(created_at) = DATE(:date)", { date: @date }).count
 
-        accumulation_start = OfficeMeta.where(key: 'reset-date').first
-        if accumulation_start
-            start_date = Date.parse accumulation_start.value
-        else
-            start_date = Document.first.created_at.beginning_of_day
-        end
-        @accumulated_own_pending = Document.alive.where("status = 1 AND created_at > :date", { date: start_date }).count
-        @accumulated_own_completed = Document.alive.where("status = 2 AND created_at > :date", { date: start_date }).count
-        @accumulated_own_rejected = Document.alive.where("status = 3 AND created_at > :date", { date: start_date }).count
-        @accumulated_routed_incoming = DocumentRoute.where("status = 0 AND created_at > :date", { date: start_date }).count
-        @accumulated_routed_pending = DocumentRoute.where("status = 1 AND created_at > :date", { date: start_date }).count
-        @accumulated_routed_released = DocumentRoute.where("status = 2 AND created_at > :date", { date: start_date }).count
-        @accumulated_routed_completed = DocumentRoute.where("status = 4 AND created_at > :date", { date: start_date }).count
-        @accumulated_routed_rejected = DocumentRoute.where("status = 3 AND created_at > :date", { date: start_date }).count
+    accumulation_start = OfficeMeta.where(key: 'reset-date').first
+    if accumulation_start
+        start_date = Date.parse accumulation_start.value
+    elsif Document.any?
+        start_date = Document.first.created_at.beginning_of_day
     else
-        @no_offices = true
+        start_data = Date.today
     end
+    @accumulated_own_pending = Document.alive.where("status = 1 AND created_at > :date", { date: start_date }).count
+    @accumulated_own_completed = Document.alive.where("status = 2 AND created_at > :date", { date: start_date }).count
+    @accumulated_own_rejected = Document.alive.where("status = 3 AND created_at > :date", { date: start_date }).count
+    @accumulated_routed_incoming = DocumentRoute.where("status = 0 AND created_at > :date", { date: start_date }).count
+    @accumulated_routed_pending = DocumentRoute.where("status = 1 AND created_at > :date", { date: start_date }).count
+    @accumulated_routed_released = DocumentRoute.where("status = 2 AND created_at > :date", { date: start_date }).count
+    @accumulated_routed_completed = DocumentRoute.where("status = 4 AND created_at > :date", { date: start_date }).count
+    @accumulated_routed_rejected = DocumentRoute.where("status = 3 AND created_at > :date", { date: start_date }).count
   end
 end
